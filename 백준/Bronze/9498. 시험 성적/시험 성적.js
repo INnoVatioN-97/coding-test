3c6a69b36ca68e982c0759c872ec7066f52320b0
Youngil Ko <koyoungil97@gmail.com>
2024-06-08 20:26:18 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,29 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+input.forEach((str) => {
+  let num = Number(str);
+  switch (Math.floor(num / 10)) {
+    case 10:
+    case 9: {
+      console.log('A');
+      break;
+    }
+    case 8: {
+      console.log('B');
+      break;
+    }
+    case 7: {
+      console.log('C');
+      break;
+    }
+    case 6: {
+      console.log('D');
+      break;
+    }
+    default: {
+      console.log('F');
+      break;
+    }
+  }
+});
